b824842b786ff8c070c205c4d70a33e80c82f983
Andrej Petras <andrej@ajka-andrej.com>
2023-12-27 13:57:19 +0100

    feat: migration (#1)

---
+++ src/test/java/io/github/onecx/iam/kc/rs/external/v1/controllers/AdminUserRestControllerIT.java
@@ -0,0 +1,8 @@
+package io.github.onecx.iam.kc.rs.external.v1.controllers;
+
+import io.quarkus.test.junit.QuarkusIntegrationTest;
+
+@QuarkusIntegrationTest
+public class AdminUserRestControllerIT extends AdminUserRestControllerTest {
+
+}
