b936deab8d712f14d511403c2b8e3efc24217335
Jorden_Reuter <149687553+JordenReuter@users.noreply.github.com>
2025-03-05 03:47:44 +0100

    fix: format

--- src/test/java/org/tkit/onecx/iam/kc/rs/internal/controllers/RealmsRestControllerTest.java
+++ src/test/java/org/tkit/onecx/iam/kc/rs/internal/controllers/RealmsRestControllerTest.java
@@ -1,48 +1,50 @@
 package org.tkit.onecx.iam.kc.rs.internal.controllers;
 
-import gen.org.tkit.onecx.iam.kc.internal.model.RealmResponseDTO;
-import io.quarkus.test.common.http.TestHTTPEndpoint;
-import io.quarkus.test.junit.QuarkusTest;
-import io.quarkus.test.keycloak.client.KeycloakTestClient;
-import jakarta.ws.rs.core.Response;
-import org.junit.jupiter.api.Test;
-import org.tkit.onecx.iam.kc.test.AbstractTest;
-import org.tkit.quarkus.security.test.GenerateKeycloakClient;
-
 import static io.restassured.RestAssured.given;
 import static jakarta.ws.rs.core.MediaType.APPLICATION_JSON;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.tkit.quarkus.security.test.SecurityTestUtils.getKeycloakClientToken;
 
+import jakarta.ws.rs.core.Response;
+
+import org.junit.jupiter.api.Test;
+import org.tkit.onecx.iam.kc.test.AbstractTest;
+import org.tkit.quarkus.security.test.GenerateKeycloakClient;
+
+import gen.org.tkit.onecx.iam.kc.internal.model.RealmResponseDTO;
+import io.quarkus.test.common.http.TestHTTPEndpoint;
+import io.quarkus.test.junit.QuarkusTest;
+import io.quarkus.test.keycloak.client.KeycloakTestClient;
+
 @QuarkusTest
 @TestHTTPEndpoint(RealmRestController.class)
-@GenerateKeycloakClient(clientName = "testClient", scopes = {"ocx-ia:read", "ocx-ia:write", "ocx-ia:all"})
+@GenerateKeycloakClient(clientName = "testClient", scopes = { "ocx-ia:read", "ocx-ia:write", "ocx-ia:all" })
 class RealmsRestControllerTest extends AbstractTest {
-  private static final KeycloakTestClient keycloakClient = new KeycloakTestClient();
+    private static final KeycloakTestClient keycloakClient = new KeycloakTestClient();
 
-  @Test
-  void findAllRealms_test() {
+    @Test
+    void findAllRealms_test() {
 
-    var result = given()
-            .auth().oauth2(getKeycloakClientToken("testClient"))
-            .contentType(APPLICATION_JSON)
-            .header(APM_HEADER_TOKEN, keycloakClient.getAccessToken(USER_BOB))
-            .get()
-            .then()
-            .statusCode(Response.Status.OK.getStatusCode())
-            .extract()
-            .body().as(RealmResponseDTO.class);
-    assertThat(result.getRealms()).hasSize(2);
-  }
+        var result = given()
+                .auth().oauth2(getKeycloakClientToken("testClient"))
+                .contentType(APPLICATION_JSON)
+                .header(APM_HEADER_TOKEN, keycloakClient.getAccessToken(USER_BOB))
+                .get()
+                .then()
+                .statusCode(Response.Status.OK.getStatusCode())
+                .extract()
+                .body().as(RealmResponseDTO.class);
+        assertThat(result.getRealms()).hasSize(2);
+    }
 
-  @Test
-  void findAllRealms_kc_exception_test() {
-    given()
-            .auth().oauth2(getKeycloakClientToken("testClient"))
-            .contentType(APPLICATION_JSON)
-            .header(APM_HEADER_TOKEN, " ")
-            .get()
-            .then()
-            .statusCode(Response.Status.BAD_REQUEST.getStatusCode());
-  }
+    @Test
+    void findAllRealms_kc_exception_test() {
+        given()
+                .auth().oauth2(getKeycloakClientToken("testClient"))
+                .contentType(APPLICATION_JSON)
+                .header(APM_HEADER_TOKEN, " ")
+                .get()
+                .then()
+                .statusCode(Response.Status.BAD_REQUEST.getStatusCode());
+    }
 }
